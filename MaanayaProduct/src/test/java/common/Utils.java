4e2bc4ddfc59fb70eb2a88cba1ce58784be7b950
nitasynergyworks-Testing <nita.synergyworks@gmail.com>
2026-08-08 08:58:45 -0700

    Delete MaanayaProduct directory

--- MaanayaProduct/src/test/java/common/Utils.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-public class Utils {
-
-    public static String generateUniqueEmail() {
-        long timestamp = System.currentTimeMillis();
-        return "Panda" + timestamp + "@gmail.com";
-    }
-}
