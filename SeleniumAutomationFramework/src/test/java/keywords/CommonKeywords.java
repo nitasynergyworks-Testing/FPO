af259662313a778a07590d086fba469d61688407
nitasynergyworks-Testing <nita.synergyworks@gmail.com>
2026-08-08 08:59:01 -0700

    Delete SeleniumAutomationFramework directory

--- SeleniumAutomationFramework/src/test/java/keywords/CommonKeywords.java
+++
@@ -1,4 +0,0 @@
-package keywords;
-
-public class CommonKeywords {
-}
--- SeleniumAutomationFramework/src/test/java/utilities/ConfigReader.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-import java.io.FileInputStream;
-import java.util.Properties;
-
-public class ConfigReader {
-}
